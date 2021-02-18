217e937c6b52d8bb6e5e6698e76df1a046beec97
MasterOfMenace <r.samotryasov@gmail.com>
2021-02-18 11:48:39 +0300

    Настроен вебпак, установлены недостающие зависимости. Произведен пробный запуск

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <div><p>HELLO WORLD</p></div>,
+    document.querySelector('#root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'public')
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    open: false,
+    publicPath: '/',
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node-modules/,
+        use: {
+          loader: 'babel-loader'
+        },
+      }
+    ]
+  },
+  devtool: 'source-map',
+  optimization: {
+    minimize: false
+  },
+  resolve: {
+    extensions: ['.js', 'jsx']
+  }
+};
