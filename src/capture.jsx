7db4182c8fb6d6fe6f7168a42f323c7db29f496f
MasterOfMenace <r.samotryasov@gmail.com>
2021-02-19 19:02:58 +0300

    Вынесены стили в scss. Добавлены пакеты node-sass, mini-sc-extract-plugin, css-loader, sass-loader, добавлены картинки, стилизованы кнопки.

--- src/capture.jsx
+++ src/capture.jsx
@@ -1,52 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
-const styles = {
-  mask: {
-    position: 'absolute',
-    left: 0,
-    top: 0,
-    width: '100%',
-    height: '100%',
-    background: 'rgba(255,255,255, 0.9)',
-    display: 'flex'
-  },
-  container: {
-    width: '320px',
-    margin: 'auto'
-  },
-  output: {
-    width: '320px',
-    height: '400px',
-    position: 'absolute',
-    left: 0,
-    top: 0
-  },
-  camera: {
-    position: 'relative',
-    background: 'black'
-  },
-  snapshotBtn: {
-    fontSize: 0,
-    width: '40px',
-    height: '40px',
-    borderRadius: '50%',
-    border: '1px solid black',
-    backgroundColor: 'white',
-    position: 'absolute',
-    left: 'calc(50% - 20px)',
-    bottom: '10%'
-  },
-  controlsWrapper: {
-    height: '50px',
-    width: '100%',
-    backgroundColor: 'rgb(0,0,0)',
-    position: 'relative',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between'
-  }
-};
+import switchIcon from './images/switch-camera.svg';
+import goBackIcon from './images/go-back-arrow.svg';
+import './styles/capture.scss';
 
 const CAMERA_TYPES = {
   USER: 'user',
@@ -63,8 +19,8 @@ class Capture extends React.Component {
     this.state = {
       streaming: false,
       width: 320,
-      height: 400,
-      imgData: null,
+      height: 0,
+      imgUrl: null,
       blob: null,
       camera: CAMERA_TYPES.ENVIRONMENT
     };
@@ -73,25 +29,37 @@ class Capture extends React.Component {
   componentDidMount = () => {
     console.log(this.state.camera);
     this.ctx = this.canvasRef.current.getContext('2d');
-    this._startup();
+    this.startup();
   }
 
-  componentWillUnmount = () => {
-    console.log('capture unmounted');
-  }
-
-  someWorkWithBlob = (blob) => {
-    const url = URL.createObjectURL(blob);
-    console.log(blob);
-    console.log(url);
-    blob.lastModified = Date.now();
+  setStreaming = (status) => {
     this.setState({
-      imgData: url,
+      streaming: status
+    });
+  }
+
+  setHeight = (height) => {
+    this.setState({
+      height
+    });
+  }
+
+  setImgUrl = (imgUrl) => {
+    this.setState({
+      imgUrl
+    });
+  }
+
+  getBlob = (blob) => {
+    const url = URL.createObjectURL(blob);
+    blob.lastModified = Date.now();
+    this.setImgUrl(url);
+    this.setState({
       blob
     });
   }
 
-  _startup = () => {
+  startup = () => {
     const video = this.videoRef.current;
     navigator.mediaDevices.getUserMedia({video: {
       facingMode: this.state.camera
@@ -105,7 +73,7 @@ class Capture extends React.Component {
     });
   }
 
-  _stopStreamedVideo = (videoElem) => {
+  stopStreamedVideo = (videoElem) => {
     const stream = videoElem.srcObject;
     const tracks = stream.getTracks();
     tracks.forEach((track) => {
@@ -114,56 +82,53 @@ class Capture extends React.Component {
     videoElem.srcObject = null;
   }
 
-  _stop = () => {
+  stop = () => {
     const video = this.videoRef.current;
-    this._stopStreamedVideo(video);
-    this._clear();
+    this.stopStreamedVideo(video);
+    this.clear();
   }
 
-  _canPlay = () => {
+  onCanPlayHandler = () => {
     const video = this.videoRef.current;
     if (!this.state.streaming) {
       const height = video.videoHeight / (video.videoWidth / this.state.width);
-      this.setState({
-        height
-      });
-
-      this.setState({
-        streaming: true
-      });
+      this.setHeight(height);
+      this.setStreaming(true);
     }
   }
 
-  _takePicture = () => {
+  takePicture = () => {
     const canvas = this.canvasRef.current;
     const video = this.videoRef.current;
     const {width, height} = this.state;
     if (width && height) {
       this.ctx.drawImage(video, 0, 0, width, height);
-
-      canvas.toBlob(this.someWorkWithBlob, 'image/jpeg'); // canvas.toBlob() returns undefined
+      canvas.toBlob(this.getBlob, 'image/jpeg');
     }
   }
 
-  _clear = () => {
+  clear = () => {
     const canvas = this.canvasRef.current;
     this.ctx.clearRect(0, 0, this.state.width, this.state.height);
 
-    const data = canvas.toDataURL('image/jpeg');
-    this.setState({
-      imgData: data
-    });
+    const url = canvas.toDataURL('image/jpeg');
+    this.setImgUrl(url);
   }
 
-  _onOkButtonClickHandler = () => {
-    this._stop();
+  onOkButtonClickHandler = () => {
+    this.stop();
     this.props.onGetData(this.state.blob);
   }
 
-  _onCancelButtonClickHandler = () => {
+  onBackButtonClickHandler = () => {
+    this.stop();
+    this.props.onBackButtonClick();
+  }
+
+  onCancelButtonClickHandler = () => {
     this.setState({
       blob: null,
-      imgData: null
+      imgUrl: null
     });
   }
 
@@ -176,33 +141,42 @@ class Capture extends React.Component {
 
   onCameraChangeButtonClickHandler = () => {
     const video = this.videoRef.current;
-    this._stopStreamedVideo(video);
-    this.toggleCamera(this.state.camera, this._startup);
+    this.stopStreamedVideo(video);
+    this.toggleCamera(this.state.camera, this.startup);
   }
 
   render = () => {
     return (
-      <div className="capture-mask" style={styles.mask}>
-        <div className="capture-container" style={styles.container}>
-          <div className="controls-wrapper" style={styles.controlsWrapper}>
-            {this.state.imgData ? <button id="okButton" onClick={this._onOkButtonClickHandler}>Ok</button> : null}
-            {this.state.imgData ? <button id="okButton" onClick={this._onCancelButtonClickHandler}>Cancel</button> : null}
+      <div className="capture-mask">
+        <div className="capture-container">
+          <div className="controls-wrapper">
+            {this.state.imgUrl ? <button className='cancel-button' onClick={this.onCancelButtonClickHandler}>Cancel</button> : null}
+            {this.state.imgUrl ? <button className='ok-button' onClick={this.onOkButtonClickHandler}>Ok</button> : null}
           </div>
-          <div className="camera" style={styles.camera}>
-            <video id="video" ref={this.videoRef} width={this.state.width} height={this.state.height} onCanPlay={this._canPlay}>Video stream not available.</video>
-            <div className="output" style={{
-              ...styles.output,
-              display: this.state.imgData ? 'block' : 'none'
+          <div className="camera">
+            <video id="video" ref={this.videoRef} width={this.state.width} height={this.state.height} onCanPlay={this.onCanPlayHandler}>Video stream not available.</video>
+            <div className="photo" style={{
+              display: this.state.imgUrl ? 'block' : 'none'
             }}>
-              <img id="photo" alt="The screen capture will appear in this box." ref={this.imageRef} src={this.state.imgData}/>
+              <img id="photo" alt="The screen capture will appear in this box." ref={this.imageRef} src={this.state.imgUrl}/>
             </div>
           </div>
-          <div className="controls-wrapper" style={styles.controlsWrapper}>
-            <button className="change-cam" onClick={this.onCameraChangeButtonClickHandler}>Change Camera</button>
-            <button id="snapshot" onClick={this._takePicture} style={styles.snapshotBtn}>Take photo</button>
-            <button id="snapshot" onClick={this.props.onBackButtonClick}>Go back</button>
+          <div className="controls-wrapper">
+            <button className="change-cam-button" onClick={this.onCameraChangeButtonClickHandler}>
+              <svg viewBox="0 0 64 64" width='32' height='32' fill='#FFFFFF'>
+                <use href={`${switchIcon}#camera-switch`} />
+              </svg>
+              <span className='visually-hidden'>Switch Camera</span>
+            </button>
+            <button className='take-photo-button' onClick={this.takePicture}>Take photo</button>
+            <button className='go-back-button' onClick={this.onBackButtonClickHandler}>
+              <svg viewBox="0 0 38 38" width='34' height='34' fill='#FFFFFF'>
+                <use href={`${goBackIcon}#go-back-arrow`} />
+              </svg>
+              <span className='visually-hidden'>Назад</span>
+            </button>
           </div>
-          <canvas ref={this.canvasRef} id="canvas" width={this.state.width} height={this.state.height} style={{display: 'none'}}></canvas>
+          <canvas ref={this.canvasRef} width={this.state.width} height={this.state.height}></canvas>
         </div>
       </div>);
   }
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,24 @@
 const path = require('path');
+const miniCss = require('mini-css-extract-plugin');
 
 module.exports = {
   mode: 'development',
+
   entry: './src/index.js',
+
   output: {
     filename: 'bundle.js',
     path: path.join(__dirname, 'public')
   },
+
   devServer: {
     contentBase: path.join(__dirname, 'public'),
     https: true,
     open: false,
     publicPath: '/',
-    port: 8080,
+    port: 3000,
   },
+
   module: {
     rules: [
       {
@@ -21,14 +26,41 @@ module.exports = {
         exclude: /node-modules/,
         use: {
           loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.(webp|jpe?g|png|gif|svg)$/,
+        use: {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'img/'
+            }
         },
+      },
+      {
+        test: /\.(s*)css$/,
+        use: [
+          miniCss.loader,
+          'css-loader',
+          'sass-loader'
+        ]
       }
     ]
   },
+
+  plugins: [
+    new miniCss({
+      filename: 'style.css'
+    }),
+  ],
+
   devtool: 'source-map',
+
   optimization: {
     minimize: false
   },
+
   resolve: {
     extensions: ['.js', '.jsx']
   }
